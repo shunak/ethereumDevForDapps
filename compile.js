c5b75266751c7dbf0d95ce5d920aab2bdcd258ec
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-07-31 23:27:22 +0900

    180731

---
+++ compile.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs'); /*fileSystemModule*/
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+solc.compile(source,1);
+
+
---
+++ test/inbox.test.js
@@ -0,0 +1,56 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async()=>{
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    
+    // Use one of those accounts to deploy
+    // the contract
+
+    describe('inbox',()=>{
+        it('deploys a contract',()=>{
+            console.log(accounts);
+        });        
+    });
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
